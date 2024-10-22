5c44cb9445d6af41e46524e31555cf6deb0c2187
zzxming <zzxming77@163.com>
2024-10-22 17:41:14 +0800

    feat: ensure log path exists

--- execute/log.ts
+++ execute/log.ts
@@ -1,5 +1,7 @@
 import { appendFileSync } from 'node:fs';
+import { ensureFileSync } from 'fs-extra';
 
 export const logAppend = (logPath: string, msg: string) => {
+  ensureFileSync(logPath);
   appendFileSync(logPath, `${msg}\n`, { encoding: 'utf8' });
 };
--- execute/replace-asar.ts
+++ execute/replace-asar.ts
@@ -5,30 +5,32 @@ import { resolve } from 'node:path';
 import { logAppend } from './log';
 
 const logPath = process.argv[2].slice(1, -1);
-if (!logPath || !existsSync(logPath)) {
-  logAppend(resolve(process.cwd(), '../..'), 'log path not exists');
-  process.exit(1);
-}
-const log = logAppend.bind(undefined, logPath);
+
 const main = async () => {
-  await new Promise(res => setTimeout(res, 2000));
-  log('start replace asar');
+  const log = logAppend.bind(undefined, logPath);
 
-  const asarDir = process.argv[3].slice(1, -1);
-  const asarPath = resolve(asarDir, 'app.asar');
-  const asarTempPath = resolve(asarDir, 'app.asar-temp');
-  if (!existsSync(asarTempPath)) {
-    throw new Error('app.asar-temp not exists');
+  try {
+    await new Promise(res => setTimeout(res, 2000));
+    log('start replace asar');
+
+    const asarDir = process.argv[3].slice(1, -1);
+    const asarPath = resolve(asarDir, 'app.asar');
+    const asarTempPath = resolve(asarDir, 'app.asar-temp');
+    if (!existsSync(asarTempPath)) {
+      throw new Error('app.asar-temp not exists');
+    }
+
+    await rm(asarPath, { force: true });
+    await rename(asarTempPath, asarPath);
+
+    log('replace asar success');
   }
-
-  await rm(asarPath, { force: true });
-  await rename(asarTempPath, asarPath);
-
-  log('replace asar success');
+  catch (error: any) {
+    log(`message: ${error.message}\n${error.stack}`);
+    log('replace asar failed');
+  }
 };
 
-main().catch((error) => {
-  log(`message: ${error.message}\n${error.stack}`);
-  log('replace asar failed');
+main().finally(() => {
   process.exit(1);
 });
